6177935d923bc7b6738edf9dd333cbceeced5a47
D-Sriver <duez.s@proton.me>
2023-06-02 15:11:09 +0200

    réorganisation du fichier Javascript.js

--- Javascript/JavaScript.js
+++ Javascript/JavaScript.js
@@ -1,25 +1,48 @@
-﻿// Création Grille
-// Son Id
-// Position x et y dans le navigateur
-// Nombre de colonne, nombre de ligne
-// Largeur colonne, largeur ligne
-// Définis la couleur de fond
-let grid = new Grid("Grid00", 50, 50, 20, 20, 15, 15, "#9ec311");
-let pomme = new Pomme();
+﻿let grid;
+let pomme;
+let snake;
 let x = 250;
 let score = 0;
 
 function start() {
-  create(grid);
+  createGrid();
+  createScore();
+  initializeSnake();
+  initializePomme();
+  startGameLoop();
+}
 
+function createGrid() {
+  // Création de la grille
+  grid = new Grid("Grid00", 50, 50, 20, 20, 15, 15, "#9ec311");
+  document.body.appendChild(grid.object);
+}
+
+function createScore() {
+  // Création de l'élément score
+  let scoreElement = document.createElement("div");
+  scoreElement.id = "Score";
+  scoreElement.innerHTML = "Score: " + score;
+  scoreElement.style.display = "flex";
+  scoreElement.style.justifyContent = "center";
+  scoreElement.style.alignItems = "center";
+  document.body.appendChild(scoreElement);
+}
+
+function initializeSnake() {
   // Création du serpent avec emplacement sur la grille.
-  let snake = new Snake(grid, grid.columns - 10, 10);
+  snake = new Snake(grid, grid.columns - 10, 10);
   snake.draw();
+}
 
+function initializePomme() {
   // Initialisation de la pomme
-  let pomme = new Pomme(grid);
+  pomme = new Pomme(grid);
   pomme.generate();
-  // ajout du mouvement avec un interval de X
+}
+
+function startGameLoop() {
+  // Ajout du mouvement avec un intervalle de X
   setInterval(() => {
     snake.move();
     snake.draw();
@@ -27,20 +50,15 @@ function start() {
     // Vérification de la collision avec la pomme
     if (pomme.checkCollision(snake)) {
       // Incrémentation du score
-      score++;
-      document.getElementById("Nombre de pomme").innerHTML =
-        "Nombre de pomme mangée : " + score;
+      score += 100;
+      updateScore();
       pomme.generate();
     }
   }, x);
 }
 
-//création de la grille
-function create(grid) {
-  document.body.appendChild(grid.object);
-
-  let scoreElement = document.createElement("div");
-  scoreElement.id = "Nombre de pomme";
-  scoreElement.innerHTML = "Nombre de pomme mangée : " + score;
-  document.body.appendChild(scoreElement);
+function updateScore() {
+  // Mettre à jour l'affichage du score
+  let scoreElement = document.getElementById("Score");
+  scoreElement.innerHTML = "Score: " + score;
 }
